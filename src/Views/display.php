99e3930d2058c76e77249b786cb37704eacafcea
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2025-03-21 23:56:07 +0330

    feat: update display view

--- src/Views/display.php
+++ src/Views/display.php
@@ -1,9 +1,9 @@
 <?php foreach ($alerts as $type => $messages): ?>
     <?php foreach ($messages as $message): ?>
-        <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, <?= $message['displayTime']; ?>)" class="toast show mt-1" role="alert" aria-live="assertive" aria-atomic="true">
+        <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, <?= esc($message['displayTime']); ?>)" class="toast show mt-1" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <span class="bg-<?= esc($type, 'attr'); ?> avatar avatar-xs me-2"></span>
-                <strong class="me-auto"><?= setting('Alerts.types')[$type] ?? ''; ?></strong>
+                <strong class="me-auto"> <?= esc($message['title'] ?? esc(setting('Alerts.types')[$type]) ?? ''); ?></strong>
                 <button class="btn-close" type="button" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body text-muted"><?= esc($message['message']); ?></div>
