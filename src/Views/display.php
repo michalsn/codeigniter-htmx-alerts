d7ff11f423aa98c06726f5de2884c93ca5358040
Pooya Parsa <pooya_parsa_dadashi@yahoo.com>
2025-03-22 12:48:15 +0330

    feat: add `withTitle()` for  custom alert title (#6)
    * docs: usage of withTitle() method for custom alert title
    
    * feat: add withTitle() method
    
    * feat: update display view
    
    * feat: update display view
    
    * fix: add title in data
    
    * fix: update display and alerts
    
    * tests: add test for withTitle() and update old test
    
    * refactor: remove esc for int value

--- src/Views/display.php
+++ src/Views/display.php
@@ -3,7 +3,7 @@
         <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, <?= $message['displayTime']; ?>)" class="toast show mt-1" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <span class="bg-<?= esc($type, 'attr'); ?> avatar avatar-xs me-2"></span>
-                <strong class="me-auto"><?= setting('Alerts.types')[$type] ?? ''; ?></strong>
+                <strong class="me-auto"><?= esc($message['title']); ?></strong>
                 <button class="btn-close" type="button" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body text-muted"><?= esc($message['message']); ?></div>
